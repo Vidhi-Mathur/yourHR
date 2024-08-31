4b33a32a00980c63ca780ad076cc13e4e2f5044b
Vidhi <mathurvidhi2505@gmail.com>
2024-08-31 14:11:48 +0530

    File stored in DropBox now

--- backend/util/file-upload.js
+++ backend/util/file-upload.js
@@ -1,16 +1,12 @@
 require('dotenv').config();
 const multer = require('multer');
+const Dropbox = require('dropbox').Dropbox;
+const fetch = require('node-fetch');
 
-// Handling file storage with multer
-const fileStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/resume/');
-    },
-    filename: (req, file, cb) => {
-        const fileExtension = file.mimetype.split('/')[1];
-        cb(null, `${Date.now()}-${file.originalname}`);
-    }
-});
+const dbx = new Dropbox({ accessToken: process.env.DROPBOX_ACCESS_TOKEN, fetch: fetch });
+
+//Handling file storage with multer, not storing locally
+const fileStorage = multer.memoryStorage()
 
 // File type filter
 const fileType = (req, file, cb) => {
@@ -22,15 +18,18 @@ const fileType = (req, file, cb) => {
 exports.upload = multer({ storage: fileStorage, fileFilter: fileType });
 
 // File upload handler
-exports.fileUpload = async (req, res) => {
+exports.fileUpload = async (req, res, next) => {
     if (!req.file) {
         return res.status(400).json({ message: 'No file uploaded' });
     }
     try {
-        return res.status(200).json({ message: 'File uploaded successfully', filePath: req.file.path });
+        const dropboxPath = `/uploads/resume/${Date.now()}-${req.file.originalname}`;
+        const response = await dbx.filesUpload({ path: dropboxPath, contents: req.file.buffer });
+        const sharedLink = await dbx.sharingCreateSharedLinkWithSettings({ path: response.result.path_lower });
+        return res.status(200).json({ filePath: sharedLink.result.url.replace('?dl=0', '?dl=1') })
     } 
-    catch (err) {
-        // Handle any errors that occur during the upload
-        return res.status(500).json({ message: 'Failed to upload file' });
+    catch(err){
+        console.log(err)
+        next(err)
     }
-};
+}
